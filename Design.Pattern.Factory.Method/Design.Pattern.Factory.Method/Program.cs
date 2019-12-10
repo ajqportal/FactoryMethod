6e2bfe98859d18c47cc792e5b4d3ae827935d095
Alvin Quezon <alvin.quezon@avensia.com>
2019-12-10 10:46:56 +0800

    initial commit

---
+++ Design.Pattern.Factory.Method/Design.Pattern.Factory.Method/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Design.Pattern.Factory.Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
